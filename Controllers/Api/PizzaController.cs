ad79728026113c7d0dc8f016c1d6ddca7baec660
Fabio Moro <fabio.moro2000@gmail.com>
2022-10-14 14:36:25 +0200

    aggiunta stampa lista pizze tramite Api

---
+++ Controllers/Api/PizzaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Policy;
+
+namespace la_mia_pizzeria_crud_mvc.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        Pizzeria context;
+
+        public PizzaController()
+        {
+            context = new Pizzeria();
+        }
+        public IActionResult Get()
+        {
+            List<Pizza> pizze = context.Pizze.ToList();
+
+            return Ok(pizze);
+        }
+    }
+}
